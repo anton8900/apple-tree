67d531ed0a3aa9d1e18ea111fec240a10c91e861
Anton Skutov <Anton@Antons-Mac.local>
2015-09-18 08:07:41 -0700

    Initial Commit

---
+++ apple tree/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  apple tree
+//
+//  Created by Anton Skutov on 9/18/15.
+//  Copyright (c) 2015 Anton Skutov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
